c84228389983de2cd725c10c37d6dab2776820e5
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-06-13 11:16:45 +0200

    H2 database, test profile, User Entity pre Mapped

--- src/main/java/com/educandoweb/springBootDemo/entities/User.java
+++ src/main/java/com/educandoweb/springBootDemo/entities/User.java
@@ -2,11 +2,20 @@ package com.educandoweb.springBootDemo.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
