68d8faeea04518ae446c55ce5951a87020dc52d3
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-06-19 11:26:01 +0200

    Added entity Payment + association OneToOne

--- src/main/java/com/educandoweb/springBootDemo/config/DemoConfig.java
+++ src/main/java/com/educandoweb/springBootDemo/config/DemoConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.springBootDemo.entities.Category;
 import com.educandoweb.springBootDemo.entities.Order;
 import com.educandoweb.springBootDemo.entities.OrderItem;
+import com.educandoweb.springBootDemo.entities.Payment;
 import com.educandoweb.springBootDemo.entities.Product;
 import com.educandoweb.springBootDemo.entities.User;
 import com.educandoweb.springBootDemo.entities.enums.OrderStatus;
@@ -46,26 +47,28 @@ public class DemoConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
 		Category cat1 = new Category(null, "Electronics");
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
-
-		userRepository.saveAll(Arrays.asList(u1, u2));
-		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
-		
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p2.getCategories().add(cat3);
@@ -81,6 +84,13 @@ public class DemoConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		// As the relation is OneToOne, orderRepository will persist. There is no PaymentRepository.
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 
