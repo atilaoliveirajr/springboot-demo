5ecd552c01eb1fb126014b1dc075604e4f5f7107
atilaoliveirajr <atila.oliveira.jr@gmail.com>
2020-06-20 13:23:51 +0200

    Added User

--- src/main/java/com/educandoweb/springBootDemo/resources/UserResource.java
+++ src/main/java/com/educandoweb/springBootDemo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.springBootDemo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.springBootDemo.Services.UserService;
 import com.educandoweb.springBootDemo.entities.User;
@@ -32,7 +36,20 @@ public class UserResource {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	// @RequestBody annotation maps the HttpRequest body to a transfer
+	// or domain object, enabling automatic deserialization of the inbound
+	// HttpRequest body onto a Java object.
+	@PostMapping
+	public ResponseEntity<User> insertUser(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 
+	
+	
 //	Testing H2
 //	@GetMapping
 //	public ResponseEntity<User> findAll2() {
